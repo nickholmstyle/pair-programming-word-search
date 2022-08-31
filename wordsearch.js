2e4ac1123a145e528ed7ff546cd0e5c8ccc346cb
nickholmstyle <nick416holm@protonmail.com>
2022-08-31 23:49:44 +0000

    pp with Tommy

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (array) {
+  // declare an empty newArray, i,j
+  let newMatrix = [];
+
+  for (j = 0; j < array[0].length; j++){
+    let newArray = [];
+    for (i = 0; i < array.length; i++ ) {
+     newArray.push(array[i][j]);
     }
+    newMatrix.push(newArray);
+  }
+return newMatrix;
+};
+
+// Checks Horizontal
+const wordSearch = (letters, word) => { 
+  let horizontalJoin = letters.map(ls => ls.join('')) 
+  for (let l of horizontalJoin) {
+      if (l.includes(word)) {
+          return true;
+      } 
+  }
+  let flipLetters = transpose(letters);
+  horizontalJoin = flipLetters.map(ls => ls.join('')) 
+  for (let l of horizontalJoin) {
+      if (l.includes(word)) {
+          return true;
+      } 
+  }
+  return false
 }
+
+// const wordSearch = (letters, word) => { 
+//     const horizontalJoin = letters.map(ls => ls.join(''))
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) return true
+//     }
+// }
 
 module.exports = wordSearch
